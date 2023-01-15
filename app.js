bfdeb906dbb165ad44dfda34ca8ca0b5086cdb7a
JTay92 <joe.r.tayler@outlook.com>
2023-01-15 15:50:07 +0000

    edit arrow function on button click to only generate userChoice once

--- app.js
+++ app.js
@@ -1,47 +1,26 @@
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
-const possibleChoices = document.querySelectorAll("button");
+const possibleChoices = document.querySelectorAll("[data-selection]");
+const playGameButton = document.getElementById("play");
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
 let userChoice;
 let computerChoice;
 let result;
+let playerScore = 0;
+let computerScore = 0;
+let playGame = true;
 
-possibleChoices.forEach((choice) =>
-  addEventListener("click", (rpsClick) => {
-    userChoice = rpsClick.target.id;
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (rpsClick) => {
+    userChoice = possibleChoice.dataset.selection;
     userChoiceDisplay.innerHTML = userChoice;
     generateCompChoice();
     getResult();
+    setTimeout(keepScore, 1000, result);
   })
 );
-
-function generateCompChoice() {
-  const randomNumber = Math.floor(Math.random() * 3 + 1); // generate random number between 1 & 3
-  console.log(randomNumber);
-
-  //   if (randomNumber === 1) {
-  //     computerChoice = "rock";
-  //   } else if (randomNumber === 2) {
-  //     computerChoice = "paper";
-  //   } else if (randomNumber === 3) {
-  //     computerChoice = "scissors";
-  //   }
-
-  switch (randomNumber) {
-    case 1:
-      computerChoice = "rock";
-      break;
-    case 2:
-      computerChoice = "paper";
-      break;
-    case 3:
-      computerChoice = "scissors";
-      break;
-    default:
-      console.log("error, this shouldn't happen");
-  }
-  computerChoiceDisplay.innerHTML = computerChoice;
-}
 
 // function to get the result of the rock, paper, scissors game
 function getResult() {
@@ -65,38 +44,38 @@ function getResult() {
 
 function keepScore(result) {
   console.log(result);
-  for (let i = 0; i < 2; i++) {
-    switch (result) {
-      case ":) You Win!!":
-        playerScore++;
-        playerScoreDisplay.innerHTML = playerScore;
-        console.log("Player + 1");
-        break;
-      case ":( You Lose!":
-        computerScore++;
-        computerScoreDisplay.innerHTML = computerScore;
-        console.log("Computer + 1");
-        break;
-      default:
-        console.log("Score not updated");
-    }
+  switch (result) {
+    case ":) You Win!!":
+      playerScore++;
+      playerScoreDisplay.innerHTML = playerScore;
+      console.log("Player + 1");
+      break;
+    case ":( You Lose!":
+      computerScore++;
+      computerScoreDisplay.innerHTML = computerScore;
+      console.log("Computer + 1");
+      break;
+    default:
+      console.log("Score not updated");
   }
 }
 
-function detButtonClick() {
-  rockButton.addEventListener("click", function () {
-    userChoice = "rock";
-    userChoiceDisplay.innerHTML = "Rock";
-    console.log("User selected Rock");
-  });
-  paperButton.addEventListener("click", function () {
-    userChoice = "paper";
-    userChoiceDisplay.innerHTML = "Paper";
-    console.log("User selected Paper");
-  });
-  scissorsButton.addEventListener("click", function () {
-    userChoice = "scissors";
-    userChoiceDisplay.innerHTML = "Scissors";
-    console.log("User selected Scissors");
-  });
+function generateCompChoice() {
+  const randomNumber = Math.floor(Math.random() * 3 + 1); // generate random number between 1 & 3
+  // console.log(randomNumber);
+
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+    default:
+      console.log("error, this shouldn't happen");
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
